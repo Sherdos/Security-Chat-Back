7dded4ae6407ee82fb8cbfaf9ee4ce4db1772411
Sherdos <sherdos26@gmail.com>
2026-05-04 16:38:10 +0600

    refactor: Rearrange imports and remove unnecessary comments in asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -1,21 +1,13 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from .routing import websocket_urlpatterns
-from .websocket_auth import JwtAuthMiddleware
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()  # <-- must be before ANY app/channels imports
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from .routing import websocket_urlpatterns
+from .websocket_auth import JwtAuthMiddleware
 
 django_asgi_app = get_asgi_application()
 
